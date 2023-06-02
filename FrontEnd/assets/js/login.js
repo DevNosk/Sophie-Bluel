5a335300343229dbd51e045dd1a86502183db8a0
Tom <Tissot.tom31@gmail.com>
2023-06-02 09:45:17 +0200

    function for token

--- FrontEnd/assets/js/login.js
+++ FrontEnd/assets/js/login.js
@@ -1,13 +1,18 @@
 const formInput = document.getElementById("form-input")
+const errorMessage = document.getElementById("error-msg-login");
 
 formInput.addEventListener("submit", function(event) {
     event.preventDefault();
     let inputEmail = document.getElementById("email").value;
     let inputPassword = document.getElementById("password").value;
     call_api_log(inputEmail, inputPassword).then(returnToken => {
-      console.log(returnToken)
+
+      console.log(returnToken);
     })
   }, true);
+  function setLocalStorageItem(key, content) {
+    localStorage.setItem(key, content);
+  }
 
 
 
